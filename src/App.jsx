c20a3f19faba892414f711d901f666a2d9aca36f
Md Imam Hussain <mihussain737@gmail.com>
2025-12-06 10:58:45 +0530

    products and productcard added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
 import './App.css'
 import { FaBeer } from 'react-icons/fa'
+import Products from './components/Products.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gray-900 ">
-      <h1 className="text-3xl  text-white font-bold ">Hello world!<FaBeer /></h1>       
-    </div>
+    <>
+      <Products/>    
+    </>
   )
 }
 
