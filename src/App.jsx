5e27c6c8961465d8c9d5251f16a869da8190cf08
Md Imam Hussain <mihussain737@gmail.com>
2025-12-17 20:47:19 +0530

    navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@ import './App.css'
 import Home from './components/home/Home.jsx'
 import Products from './components/products/Products.jsx'
 import { BrowserRouter as Router , Routes, Route} from 'react-router-dom'
+import Navbar from './components/shared/Navbar.jsx'
 
 function App() {
 
   return (
     <Router>
+      <Navbar></Navbar>
       <Routes>
         <Route path='/' element={<Home/>}/>
         <Route path='/products' element={<Products/>}/>
