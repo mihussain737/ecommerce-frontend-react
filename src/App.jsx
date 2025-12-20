7fa8ceed227a6fc51f708aa09b5f1b5a4e650426
Md Imam Hussain <mihussain737@gmail.com>
2025-12-20 21:11:16 +0530

    cart page added

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import About from "./components/About.jsx";
 import Contact from "./components/Contact.jsx";
 import { Toaster } from "react-hot-toast";
 import React from "react";
+import Cart from "./components/cart/Cart.jsx";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path="/products" element={<Products />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
       </Router>
       <Toaster position="bottom-center" />
