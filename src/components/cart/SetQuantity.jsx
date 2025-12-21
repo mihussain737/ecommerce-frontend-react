9260709729e9995e47a7632652d57f6a34bf4bfb
Md Imam Hussain <mihussain737@gmail.com>
2025-12-21 20:34:07 +0530

    cart item added

---
+++ src/components/cart/SetQuantity.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+
+const btnStyles="border-[1.2px] border-slate-800 px-3 py-1 rounded";
+
+const SetQuantity = ({
+     quantity,
+     cardCounter,
+     handleQtyIncrease,
+     handleQtyDecrease,
+}) => {
+  return (
+    <div className='flex gap-8 items-center'>
+     {cardCounter ? null: <div className='semi-bold'> QUANTITY</div>}
+          <div className='flex md:flex-row flex-col items-center lg:text-[22px] text-sm gap-6'>
+               <button disabled={quantity<=1}
+                    className={btnStyles}>
+                         -
+               </button>
+               <div className='text-red-600 font-semibold'>
+                    {quantity}
+               </div>
+               <button disabled={quantity<=1}
+                    className={btnStyles}>
+                    +
+               </button>
+          </div>
+    </div>
+  )
+}
+
+export default SetQuantity
