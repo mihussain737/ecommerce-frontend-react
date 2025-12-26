a1005c5dc87e08ebe872a49ac6f0f75c8cb757a3
Md Imam Hussain <mihussain737@gmail.com>
2025-12-26 18:53:18 +0530

    input field added dynamic

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import Contact from "./components/Contact.jsx";
 import { Toaster } from "react-hot-toast";
 import React from "react";
 import Cart from "./components/cart/Cart.jsx";
+import Login from "./components/auth/Login.jsx";
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </Router>
       <Toaster position="bottom-center" />
