1fa122f413b2a18ff856edabe07092f027fd954f
Md Imam Hussain <mihussain737@gmail.com>
2025-12-27 21:41:12 +0530

    welcome removed and avatar added on navbar

---
+++ src/components/UserMenu.jsx
@@ -0,0 +1,41 @@
+import { Avatar, Button, Menu, MenuItem } from '@mui/material';
+import React from 'react'
+
+const UserMenu = () => {
+ const [anchorEl, setAnchorEl] = React.useState(null) ;
+  const open = Boolean(anchorEl);
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <div
+        className='sm:border-[1px] sm:border-slate-400 flex flex-row items-center gap-1 rounded-full cursor-pointer hover:shadow-md transition text-slate-700'
+        onClick={handleClick}
+      >
+        <Avatar alt='menu' src=''/>
+      </div>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        slotProps={{
+          list: {
+            'aria-labelledby': 'basic-button',
+          },
+        }}
+      >
+        <MenuItem onClick={handleClose}>Profile</MenuItem>
+        <MenuItem onClick={handleClose}>My account</MenuItem>
+        <MenuItem onClick={handleClose}>Logout</MenuItem>
+      </Menu>
+    </div>
+  );
+}
+
+export default UserMenu
