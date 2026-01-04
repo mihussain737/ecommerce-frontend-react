741c3a3899a5e9dc2288057e2db570d520d397ae
Md Imam Hussain <mihussain737@gmail.com>
2026-01-04 19:51:25 +0530

    back and next button added in checkout

--- src/components/checkout/Checkout.jsx
+++ src/components/checkout/Checkout.jsx
@@ -1,39 +1,95 @@
-import { Step, StepLabel, Stepper } from "@mui/material";
+import {
+  Button,
+  checkboxClasses,
+  Step,
+  StepLabel,
+  Stepper,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import AddressInfo from "./AddressInfo";
 import { useDispatch, useSelector } from "react-redux";
 import { getUserAddress } from "../../store/actions";
-
+import toast from "react-hot-toast";
 
 const Checkout = () => {
+  const [activeStep, setActiveStep] = useState(0);
+  const dispatch = useDispatch();
+  const { address, selectedUserCheckoutAddress } = useSelector(
+    (state) => state.auth
+  );
+  const { isLoading, errorMessage } = useSelector((state) => state.errors);
+  const paymentMethod = false;
 
-     const [activeStep, setActiveStep]=useState(0);
-     const dispatch=useDispatch();
-     const {address}=useSelector((state)=>state.auth)
+  const handleBack = () => {
+    setActiveStep((prevStep) => prevStep - 1);
+  };
 
-     const steps=[
-          "Address",
-          "Payment Method",
-          "Order Summary",
-          "Payment"
-     ];
-     useEffect(()=>{
-          dispatch(getUserAddress());
-     },[dispatch])
+  const handleNext = () => {
+     if(activeStep===0 && !selectedUserCheckoutAddress){
+          toast.error("Please select checkout address before proceeding.");
+          return;
+     }
+
+     if(activeStep===1 && (!selectedUserCheckoutAddress || !paymentMethod)){
+          toast.error("Please select checkout address before proceeding.");
+          return;
+     }
+    setActiveStep((prevStep) => prevStep + 1);
+  };
+
+  const steps = ["Address", "Payment Method", "Order Summary", "Payment"];
+  useEffect(() => {
+    dispatch(getUserAddress());
+  }, [dispatch]);
   return (
     <div className="py-14 min-h-[calc(100vh-100px)]">
       <Stepper activeStep={activeStep} alternativeLabel>
-          {steps.map((label,index)=>
-               <Step key={index}>
-                    <StepLabel>
-                         {label}
-                    </StepLabel>
-               </Step>
-          )}
+        {steps.map((label, index) => (
+          <Step key={index}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
       </Stepper>
 
       <div className="mt-5">
-          {activeStep ===0 && <AddressInfo address={address}/>}     
+        {activeStep === 0 && <AddressInfo address={address} />}
+      </div>
+
+      <div
+        className="flex justify-between items-center px-4 fixed z-50 h-15 bottom-0 bg-white left-0 w-full py-4 border-slate-200"
+        style={{ boxShadow: "0 -2px 4px rgba(100,100,0.15)" }}
+      >
+        <Button
+          variant="outlined"
+          disabled={activeStep === 0}
+          onClick={handleBack}
+        >
+          Back
+        </Button>
+
+        {activeStep !== steps.length - 1 && (
+          <button
+            disabled={
+              errorMessage ||
+              (activeStep === 0
+                ? !selectedUserCheckoutAddress
+                : activeStep === 1
+                ? !paymentMethod
+                : false)
+            }
+            className={`bg-blue-600 font-semibold px-6 h-10 rounded-md text-white 
+                    ${
+                      errorMessage ||
+                      (activeStep === 0 && !selectedUserCheckoutAddress) ||
+                      (activeStep === 1 && !paymentMethod)
+                        ? "opacity-60"
+                        : ""
+                    }`}
+                    onClick={handleNext}
+          >
+            Proceed
+          </button>
+        )}
       </div>
     </div>
   );
